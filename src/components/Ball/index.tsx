69509ef3d68d0cb4cb8ea7eb23e07aa0abc2cf3e
khanglv-vuatho <khanglv@vuatho.com>
2024-08-29 13:58:54 +0700

    updadte

--- src/components/Ball/index.tsx
+++ src/components/Ball/index.tsx
@@ -4,34 +4,34 @@ import BackgroundGradient from '../BackgroundGradient'
 
 const Ball = ({ item, className }: { item: string; className?: string }) => {
   return (
-    // <BackgroundGradient isHiddenAnimation={!item.includes('?')}>
-    <div
-      className={twMerge('flex size-[38px] items-center justify-center gap-[1px] rounded-full text-sm font-bold', className)}
-      style={{
-        background: 'radial-gradient(100% 100% at 0% 0%, #FFFFFF 0%, #FFFFFF 70.5%, #BFBFBF 100%)'
-      }}
-    >
-      {item.split('').map((item, index) =>
-        item === '?' ? (
-          <span
-            style={
-              {
-                // borderStyle: 'inset'
+    <BackgroundGradient isHiddenAnimation={!item.includes('?')}>
+      <div
+        className={twMerge('flex size-[38px] items-center justify-center gap-[1px] rounded-full text-sm font-bold', className)}
+        style={{
+          background: 'radial-gradient(100% 100% at 0% 0%, #FFFFFF 0%, #FFFFFF 70.5%, #BFBFBF 100%)'
+        }}
+      >
+        {item.split('').map((item, index) =>
+          item === '?' ? (
+            <span
+              style={
+                {
+                  // borderStyle: 'inset'
+                }
               }
-            }
-            key={index}
-            className='rounded-[2px] border border-[#a6a6a6] px-[2px] text-primary-black'
-          >
-            {item}
-          </span>
-        ) : (
-          <span key={index} className='font-bold text-primary-black'>
-            {item}
-          </span>
-        )
-      )}
-    </div>
-    // </BackgroundGradient>
+              key={index}
+              className='rounded-[2px] border border-[#a6a6a6] px-[2px] text-primary-black'
+            >
+              {item}
+            </span>
+          ) : (
+            <span key={index} className='font-bold text-primary-black'>
+              {item}
+            </span>
+          )
+        )}
+      </div>
+    </BackgroundGradient>
   )
 }
 
