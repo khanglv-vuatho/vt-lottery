17338f5d7e8348e6f77268943d405bb8f222ff54
khanglv-vuatho <khanglv@vuatho.com>
2024-08-29 14:02:35 +0700

    update

--- src/components/BackgroundGradient/index.tsx
+++ src/components/BackgroundGradient/index.tsx
@@ -3,11 +3,12 @@ import { twMerge } from 'tailwind-merge'
 
 const BackgroundGradient = ({ children, className, isHiddenAnimation }: { children: React.ReactNode; className?: string; isHiddenAnimation?: boolean }) => {
   return (
-    <div className={twMerge('relative', className)}>
+    <div className={twMerge('relative z-50', className)}>
       <motion.div
-        animate={{ rotate: 360, scale: [1, 1.2, 0.9], zIndex: -1 }}
+        initial={{ opacity: 1 }}
+        animate={{ opacity: 1, rotate: 360, scale: [1, 1.2, 0.9] }}
         transition={{ duration: 1.5, repeat: Infinity, ease: 'easeInOut' }}
-        className={twMerge('absolute inset-0 rounded-full bg-white blur-sm', isHiddenAnimation ? 'hidden' : '')}
+        className={twMerge('absolute inset-0 z-[-1] rounded-full bg-white blur-sm', isHiddenAnimation ? 'hidden' : '')}
       />
       {children}
     </div>
